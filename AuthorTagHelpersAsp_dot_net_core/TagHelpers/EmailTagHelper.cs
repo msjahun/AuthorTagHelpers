493356694b403f9c9cbd014a714a6461f04209f9
Musa Jahun <msjahun@live.com>
2018-08-29 19:48:09 +0300

    tested out Output.Attributes.SetAttributes methods, in Email tag helper and Output.Attributes.Add methods

--- AuthorTagHelpersAsp_dot_net_core/TagHelpers/EmailTagHelper.cs
+++ AuthorTagHelpersAsp_dot_net_core/TagHelpers/EmailTagHelper.cs
@@ -15,6 +15,8 @@ namespace AuthorTagHelpersAsp_dot_net_core.TagHelpers
 
             output.TagName = "a"; //Replaces <email> tag with <a> anchor tag
             var address = MailTo + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Attributes.Add(new TagHelperAttribute("class", "btn btn-danger"));
             output.Content.SetContent(address);
         }
     }
