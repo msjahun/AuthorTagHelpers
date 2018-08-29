6d1e918ac2cd3ac065a28887c5b707f39aa77cf4
Musa Jahun <msjahun@live.com>
2018-08-29 19:48:46 +0300

    added MenuLinkTagHelper and tested it out, it actually generated exactly what I wanted it to generate

---
+++ AuthorTagHelpersAsp_dot_net_core/TagHelpers/MenuLinkTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AuthorTagHelpersAsp_dot_net_core.TagHelpers
+{
+    public class MenuLinkTagHelper : TagHelper
+    {
+        
+            public string Controller { get; set; }
+        public string Action { get; set; }
+        public string DisplayName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+            var content = $"<a href=\"/{Controller}/{Action }\"><i class=\"fa fa-circle-o\"></i> {DisplayName}</a>";
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
