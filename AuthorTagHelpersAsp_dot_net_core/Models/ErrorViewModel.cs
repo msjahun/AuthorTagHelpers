76541d3c3a268e11a8f02afd3da2bf4185da7609
Musa Jahun <msjahun@live.com>
2018-08-29 19:10:14 +0300

    Add project files.

---
+++ AuthorTagHelpersAsp_dot_net_core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthorTagHelpersAsp_dot_net_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
