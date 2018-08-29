3362c4d5e157dc94118fa16ecc9af6a09d908a5e
Musa Jahun <msjahun@live.com>
2018-08-29 19:57:20 +0300

    created websiteContextModel

---
+++ AuthorTagHelpersAsp_dot_net_core/Models/WebsiteContextModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorTagHelpersAsp_dot_net_core.Models
+{
+    public class WebsiteContextModel
+    {
+        public Version Version { get; set; }
+        public int CopyrightYear { get; set; }
+        public bool Approved { get; set; }
+        public int TagsToShow { get; set; }
+    }
+}
