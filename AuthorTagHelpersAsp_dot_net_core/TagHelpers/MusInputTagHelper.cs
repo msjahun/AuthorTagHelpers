3bf63485fc20250645b4a4c70eb02dc4d03ecc22
Musa Jahun <msjahun@live.com>
2018-09-05 16:04:47 +0300

    created another tagHelper MusInput <mus-inpu></mus-input> TagHelper, this taghelper inherits from the InputFactory taghelper so it is able to interact wit the context Model

---
+++ AuthorTagHelpersAsp_dot_net_core/TagHelpers/MusInputTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorTagHelpersAsp_dot_net_core.TagHelpers
+{
+    public class MusInputTagHelper : InputTagHelper
+    {
+        public MusInputTagHelper(IHtmlGenerator generator):base(generator)
+        {
+                    
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //tag details
+            output.TagName = "input";
+            output.TagMode = TagMode.StartTagOnly;
+
+            output.Attributes.SetAttribute("class","form-control");
+            output.Attributes.SetAttribute("type","text");
+            output.Attributes.SetAttribute("musa","musa");
+            base.Process(context, output);
+        }
+    }
+}
