1cd4cea1fb7dad1beb71a28a9e324f07e3819299
Musa Jahun <msjahun@live.com>
2018-09-05 16:03:44 +0300

    added a custom tagHelper DauInput <dau-input></dau-input> tagHelper, but I couldn't get it to interact with the Modal

---
+++ AuthorTagHelpersAsp_dot_net_core/TagHelpers/DauInputTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorTagHelpersAsp_dot_net_core.TagHelpers
+{
+    public class DauInputTagHelper : TagHelper
+    {
+
+      
+        public string Type { get; set; }
+        public ModelExpression AspFor { get; set; }
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div"; //Replaces <email> tag with <a> anchor tag
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var sb = new StringBuilder();
+         
+            sb.AppendFormat("<input  type=\"{0}\" class=\"form-control\">{1}</input>", this.Type, this.AspFor.Name);
+            sb.AppendFormat("<span  class=\"glyphicon glyphicon-lock form-control-feedback\"></span>");
+           
+
+            output.Content.SetHtmlContent(sb.ToString());
+        }
+    }
+}
