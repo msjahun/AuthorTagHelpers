3e3afe90c99c62b6e49bf272123ee295d3149248
Musa Jahun <msjahun@live.com>
2018-09-05 16:02:26 +0300

    Added a login controller and index action that validates Modelstate

---
+++ AuthorTagHelpersAsp_dot_net_core/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorTagHelpersAsp_dot_net_core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorTagHelpersAsp_dot_net_core.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+
+
+                return RedirectToAction("Index", "Home");
+
+
+            }
+
+
+            return View(vm);
+        }
+
+
+    }
+}
