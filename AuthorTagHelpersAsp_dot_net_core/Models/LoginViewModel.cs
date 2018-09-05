ff12f1ac9d6332943ddac2b2663f23caa8cbb802
Musa Jahun <msjahun@live.com>
2018-09-05 16:03:06 +0300

    Added a login view model with annotations

---
+++ AuthorTagHelpersAsp_dot_net_core/Models/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AuthorTagHelpersAsp_dot_net_core.Models
+{
+    public class LoginViewModel
+    {
+
+
+        [Required, EmailAddress, MaxLength(256), Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Password")]
+
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl
+        {
+            get; set;
+        }
+    }
+}
