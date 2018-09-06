071672747839caab9de91c7f5e2d7616021dd9f6
Musa Jahun <msjahun@live.com>
2018-09-06 12:44:11 +0300

    Created the ModalContext class, its sort of like a viewModel for taghelpers

---
+++ AuthorTagHelpersAsp_dot_net_core/Models/ModalContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorTagHelpersAsp_dot_net_core.Models
+{
+    public class ModalContext
+    {
+        public IHtmlContent Body { get; set; }
+        public IHtmlContent Footer { get; set; }
+    }
+
+}
